e3f608536c7ca55bbafa9c9488e32557a16b7bde
Jasurbek Kalandarov <jkalandarov@gmail.com>
2023-01-28 15:30:13 +0500

    finished user singning in

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import { validateRequest } from "../middlewares/validate-request";
+import { User } from "../models/user";
+import { BadRequestError } from "../errors/bad-request-error";
+import { Password } from "../services/password";
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -15,7 +19,28 @@ const bodyValidation = [
 ];
 
 router.post('/api/users/signin', bodyValidation, validateRequest, async (req: Request, res: Response) => {
+  const { email, password } = req.body;
   
+  const existingUser = await User.findOne({ email });
+  if(!existingUser) {
+    throw new BadRequestError('Invalid credentials');
+  }
+
+  const passwordsMatch = await Password.compare(existingUser.password, password);
+  if(!passwordsMatch) {
+    throw new BadRequestError('Invalid credentials');
+  }
+
+  const userJwt = jwt.sign(
+    { 
+      id: existingUser.id, 
+      email: existingUser.email 
+    }, 
+    process.env.JWT_KEY!
+  );
+
+  req.session = { jwt: userJwt };
+  res.status(201).send(existingUser);
 });
 
 export { router as signinRouter }
