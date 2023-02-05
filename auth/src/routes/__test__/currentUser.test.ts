f978d2e00941926fc7411f790be5ba63f133a4ee
Jasurbek Kalandarov <jkalandarov@gmail.com>
2023-02-05 12:45:43 +0500

    refactored singIn for testing

--- auth/src/routes/__test__/currentUser.test.ts
+++ auth/src/routes/__test__/currentUser.test.ts
@@ -11,14 +11,7 @@ interface Response {
 }
 
 it('responds with details about the current user', async () => {
-  const authResponse = await request(app)
-    .post('/api/users/signup')
-    .send({
-      email: 'test@test.com',
-      password: 'password'
-    })
-    .expect(201);
-  const cookie = authResponse.get('Set-Cookie');
+  const cookie = await signin();
 
   const response: Response = await request(app)
     .get('/api/users/currentuser')
@@ -27,4 +20,13 @@ it('responds with details about the current user', async () => {
     .expect(200);
   
   expect(response.body.currentUser.email).toEqual('test@test.com');
+});
+
+it('responds with null if not authenticated', async () => {
+  const response: Response = await request(app)
+    .get('/api/users/currentuser')
+    .send()
+    .expect(200);
+  
+  expect(response.body.currentUser).toEqual(null);
 });
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,9 +1,16 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import { app } from '../app';
+import request from 'supertest';
+
+declare global {
+  function signin(): Promise<string[]>
+}
 
 let mongo: any;
 beforeAll(async () => {
   process.env.JWT_KEY = 'AaSsDd123';
+  jest.setTimeout(10000);
 
   mongo = await MongoMemoryServer.create();
   const mongoUri = mongo.getUri();
@@ -21,3 +28,16 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({ email, password })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+  return cookie;
+}
