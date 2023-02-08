eca7e15baab5b1dad5958deef7193cb770586abb
Jasurbek Kalandarov <jkalandarov@gmail.com>
2023-02-08 17:50:33 +0500

    refactored default api call on home page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,25 +1,14 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  return <h1>Landing Page</h1>
+  return currentUser
+    ? <h1>You are signed in</h1>
+    : <h1>You are NOT signed in</h1>
 }
 
-LandingPage.getInitialProps = async () => {
-  if (typeof window === 'undefined') {
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: {
-          Host: 'ticketing.dev'
-        }
-      }
-    );
-    return data;
-  } else {
-    const { data } = await axios.get('/api/users/currentuser');
-    return data;
-  }
+LandingPage.getInitialProps = async (context) => {
+  const { data } = await buildClient(context).get('/api/users/currentuser');
+  return data;
 }
 
 export default LandingPage;
