9231669d3a4abdf0fc3c09d8e8d2dd57400fe0b2
Jasurbek Kalandarov <jkalandarov@gmail.com>
2023-02-19 12:19:19 +0500

    refactored nats listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -14,28 +14,7 @@ stan.on('connect', () => {
     console.log('NATS connection closed...');
     process.exit();
   });
-
-  const options = stan
-    .subscriptionOptions()
-    .setManualAckMode(true)
-    .setDeliverAllAvailable()
-    .setDurableName('service-name');
-
-  const subscription = stan.subscribe(
-    'ticket:created',
-    'queue-group-name',
-    options
-  );
-  
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData();
-    
-    if (typeof data === 'string') {
-      console.log(`Received event number ${msg.getSequence()}, with data: ${data}`);
-    }
-
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 process.on('SIGINT', () => stan.close());
@@ -83,5 +62,16 @@ abstract class Listener {
     return typeof data === 'string'
       ? JSON.parse(data)
       : JSON.parse(data.toString('utf-8'));
+  }
+}
+
+class TicketCreatedListener extends Listener {
+  subject = 'ticket:created';
+  queueGroupName = 'payment-service';
+
+  onMessage(data: any, msg: Message) {
+    console.log('Event data: ', data);
+    
+    msg.ack();
   }
 }
