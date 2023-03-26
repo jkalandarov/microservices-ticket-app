790e0bc327593ddd68902031c97ad8ea17adbe2f
Jasurbek Kalandarov <jkalandarov@gmail.com>
2023-03-26 13:42:11 +0500

    created routes for order service

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -3,10 +3,10 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@sgtickets/common';
-import { createTicketRouter } from './routes/new';
-import { indexTicketRouter } from './routes';
-import { showTicketRouter } from './routes/show';
-import { updateTicketRouter } from './routes/update';
+import { newOrderRouter } from './routes/new';
+import { indexOrderRouter } from './routes';
+import { showOrderRouter } from './routes/show';
+import { deleteOrderRouter } from './routes/delete';
 
 const app = express();
 app.set('trust proxy', true);
@@ -18,10 +18,10 @@ app.use(
   })
 );
 app.use(currentUser);
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(newOrderRouter);
+app.use(indexOrderRouter);
+app.use(showOrderRouter);
+app.use(deleteOrderRouter);
 
 app.all('*', async (req: Request, res: Response) => {
   throw new NotFoundError();
